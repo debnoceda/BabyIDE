07c4e782435f1fc73af2bc1140cd42400ebbf1cb
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2023-10-11 13:52:08 +0800

    Create separate java classes
    A main class is added together with HomeScreen and LoadingScreen panels for more readability.

--- Baby/Baby/Baby/src/main/java/packages/baby/homeScreen.java
+++ Baby/Baby/Baby/src/main/java/packages/baby/homeScreen.java
@@ -8,12 +8,12 @@ package packages.baby;
  *
  * @author Dave Noceda
  */
-public class homeScreen extends javax.swing.JFrame {
+public class HomeScreen extends javax.swing.JFrame {
 
     /**
      * Creates new form homeScreen
      */
-    public homeScreen() {
+    public HomeScreen() {
         initComponents();
     }
 
@@ -99,20 +99,21 @@ public class homeScreen extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(homeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(homeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(homeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(homeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new homeScreen().setVisible(true);
+                new HomeScreen().setVisible(true);
             }
         });
     }
