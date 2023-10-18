b4024b23b11273af5fefdfe559794981c69b6a55
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2023-10-19 01:43:19 +0800

    Fix undo/redo issues

--- Baby/Baby/Baby/src/main/java/packages/baby/frames/Ide.java
+++ Baby/Baby/Baby/src/main/java/packages/baby/frames/Ide.java
@@ -593,7 +593,8 @@ public class Ide extends javax.swing.JFrame {
         }
         
         editor.open();
-        updateFileName();  
+        updateFileName();
+        editor.resetUndoManager();
     }//GEN-LAST:event_OpenActionPerformed
 
     private void newWindowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newWindowActionPerformed
@@ -616,7 +617,8 @@ public class Ide extends javax.swing.JFrame {
         }
         
         editor.open();
-        updateFileName();  
+        updateFileName();
+        editor.resetUndoManager();
     }//GEN-LAST:event_openFileActionPerformed
 
     private void undoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_undoActionPerformed
@@ -650,7 +652,8 @@ public class Ide extends javax.swing.JFrame {
         }
         
         editor.close();
-        updateFileName();  
+        updateFileName();
+        editor.resetUndoManager();
     }//GEN-LAST:event_closeFileActionPerformed
 
     private void saveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveActionPerformed
