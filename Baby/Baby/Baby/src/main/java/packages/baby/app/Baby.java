11f2776fdb803afae77d3b2d7271a4d60d32110b
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2023-10-18 13:57:17 +0800

    Switch main to Baby.java

---
+++ Baby/Baby/Baby/src/main/java/packages/baby/app/Baby.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package packages.baby.app;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import packages.baby.components.CodeFile;
+import packages.baby.frames.HomeScreen;
+import packages.baby.frames.Ide;
+
+/**
+ *
+ * @author liaminakigillamac
+ */
+public class Baby {
+    public static void main (String[] args){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        // Check if the operating system is macOS
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            
+        }
+              
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new HomeScreen().setVisible(true);
+            }
+        });
+    }
+}
--- Baby/Baby/Baby/src/main/java/packages/baby/frames/HomeScreen.java
+++ Baby/Baby/Baby/src/main/java/packages/baby/frames/HomeScreen.java
@@ -123,55 +123,6 @@ public class HomeScreen extends javax.swing.JFrame {
         dispose(); //closes the current frame
     }//GEN-LAST:event_openFileActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HomeScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        
-
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        // Check if the operating system is macOS
-        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-            System.setProperty("apple.laf.useScreenMenuBar", "true");
-            
-        }
-              
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new HomeScreen().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Logo;
